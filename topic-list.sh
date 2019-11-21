8aa78b1f70b6b8788ef541f97faec4a177361f78
YODEL\Lee.Richards <lee.richards@yodel.co.uk>
2019-11-21 09:40:39 +0000

    Add topic describe

--- topic-list.sh
+++ topic-list.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
+# Loop optional arguments
+topicList=()
+while :; do
+	case $1 in
+		-t|--topic)
+			if [ "$2" ]; then
+				topicList+=($2)
+				shift
+			else
+				echo 'ERROR: "--topic" requires a non-empty argument'
+				exit 1
+			fi
+			;;
+		*)
+			break
+	esac
+	shift
+done
+
 # Check argument count
 if [ $# -ne 1 ]
 then
-	echo "Usage: topic-list PROPERTIES_FILE"
+	echo "Usage: topic-list [-t TOPIC]... PROPERTIES_FILE"
 	exit 1
 fi
 
@@ -21,12 +40,52 @@ then
 	exit 1
 fi
 
+# Output arguments
 echo $0 $1
+for searchTopic in "${topicList[@]}"
+do
+	echo "searchTopic:" $searchTopic
+done
 echo "zookeeper:" ${zookeeper}
 
+# Get list of all topics
 docker exec broker \
   kafka-topics \
     --list \
     --zookeeper ${zookeeper} > topic-list.log
+
+# Read file into array
+mapfile -t < topic-list.log
+
+# Find topics
+rm found-topic-list.log
+for foundTopic in "${MAPFILE[@]}"
+do
+ 	foundTopicUpper=$(echo $foundTopic | tr [a-z] [A-Z])
+	for searchTopic in "${topicList[@]}"
+    do
+		searchTopicUpper=$(echo $searchTopic | tr [a-z] [A-Z])
+		if [[ $foundTopicUpper =~ $searchTopicUpper ]]; then
+			echo $foundTopic >> found-topic-list.log
+		fi
+	done
+done
+
+# Sort File
+sort found-topic-list.log > found-topic-list-sorted.log
+
+# Read file into array
+mapfile -t < found-topic-list-sorted.log
+
+# Describe each topic
+rm describe-topic-list.log
+for topic in "${MAPFILE[@]}"
+do
+	docker exec broker \
+  		kafka-topics \
+    		--describe \
+    		--topic ${topic} \
+    		--zookeeper ${zookeeper} >> describe-topic-list.log
+done
 
 exit 0
